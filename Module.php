0f6241136e93d5aa7a579f4ed1bf98478dfbccfc
afsolarte <devsolarte@gmail.com>
2015-10-05 21:31:45 -0500

    Primer commit

---
+++ Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace vendor\afsolarte\onixCms;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'vendor\afsolarte\onixCms\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
